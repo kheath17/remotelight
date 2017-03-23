baad5f37938086d1160acb382aee03377b945476
kheath17 <kheath17@vt.edu>
2017-03-23 11:28:33 -0400

    TRANSFORM

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,13 @@
+fil = new Array();
+fil["0"]= "c-disclaimer.html@@@Disclaimer@@@Remote Network Lighting will not be responsible for any electrical problems that may occur after use...";
+fil["1"]= "how-to-program-bulbs-to-groups.html@@@Programming Light Bulbs to Lighting Groups@@@You can program one or more light bulbs to a lighting group to operate that group with your remote control...";
+fil["2"]= "how-to-turn-on-off-dim-lights.html@@@Turning On/Off or Dimming a Lights@@@You can turn on, turn off, or dim a lighting group by pressing the group number button and then pressing the power button or the dimmer control...";
+fil["3"]= "intro-considerations-planning-light-network.html@@@Considerations for Planning Your Lighting Network@@@Before installing your network lighting, you should plan how you want your lighting network to behave...";
+fil["4"]= "intro-max-number-bulbs.html@@@Maximum Number of Network Light Bulbs@@@Testing shows you should network no more than 250 light bulbs to maintain reliable performance...";
+fil["5"]= "intro-network-lighting.html@@@Basic Concepts of Network Lighting@@@You can network LED light bulbs together to operate wirelessly from a remote control...";
+fil["6"]= "intro-product.html@@@Remote Lighting Overview@@@Remote lighting allows you to operate network-based home lighting through a remote control...";
+fil["7"]= "ref-remote-control-components.html@@@Remote Control Components@@@The remote control has components that allow you to program and operate the light bulbs on your lighting network...";
+fil["8"]= "ref-specifications-led-bulb.html@@@LED Light Bulb Specifications@@@Specifications describing the electronics and technology inside the LED light bulb...";
+fil["9"]= "ref-specifications-overview.html@@@Product Specifications@@@The network lighting components have specific electronic and technical specifications...";
+fil["10"]= "ref-specifications-remote-control.html@@@Remote Control Specifications@@@Specifications describing the electronics and technology inside the remote control...";
+fil["11"]= "ts-restart.html@@@***@@@If the remote is not controlling the lights then these may be the causes. The connection may have been lost. The remote may not be activated yet. Restart the Remote Network Lighting by, holding down...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,15 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "c-disclaimer.html";
+fl["1"]= "how-to-program-bulbs-to-groups.html";
+fl["2"]= "how-to-turn-on-off-dim-lights.html";
+fl["3"]= "intro-considerations-planning-light-network.html";
+fl["4"]= "intro-max-number-bulbs.html";
+fl["5"]= "intro-network-lighting.html";
+fl["6"]= "intro-product.html";
+fl["7"]= "ref-remote-control-components.html";
+fl["8"]= "ref-specifications-led-bulb.html";
+fl["9"]= "ref-specifications-overview.html";
+fl["10"]= "ref-specifications-remote-control.html";
+fl["11"]= "ts-restart.html";
+var doStem = false;searchLoaded = true;
