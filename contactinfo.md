2f71298c40b9f269880e705af4578ad0a810a973
kheath17 <kheath17@vt.edu>
2017-03-28 10:25:52 -0400

    contactinfo.md and transform in docs added

---
+++ contactinfo.md
@@ -0,0 +1,15 @@
+# Contact Information
+
+If you need assistance with your remote or would like to register a concern, refer to the company cotacts below.
+
+## Company Support
+
+* Product Support: 1-800-REM-LIGHT
+* Customer Services: 1-800-234-HELP
+
+
+If you need electrical help, you can put your zip code into the search bar at this site for a reference to a local electrician.
+
+## Electrical Support
+
+* www.localelectricians.com
--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -1,15 +1,16 @@
 //List of files which are indexed.
 fl = new Array();
 fl["0"]= "c-disclaimer.html";
-fl["1"]= "how-to-program-bulbs-to-groups.html";
-fl["2"]= "how-to-turn-on-off-dim-lights.html";
-fl["3"]= "intro-considerations-planning-light-network.html";
-fl["4"]= "intro-max-number-bulbs.html";
-fl["5"]= "intro-network-lighting.html";
-fl["6"]= "intro-product.html";
-fl["7"]= "ref-remote-control-components.html";
-fl["8"]= "ref-specifications-led-bulb.html";
-fl["9"]= "ref-specifications-overview.html";
-fl["10"]= "ref-specifications-remote-control.html";
-fl["11"]= "ts-restart.html";
+fl["1"]= "contactinfo.html";
+fl["2"]= "how-to-program-bulbs-to-groups.html";
+fl["3"]= "how-to-turn-on-off-dim-lights.html";
+fl["4"]= "intro-considerations-planning-light-network.html";
+fl["5"]= "intro-max-number-bulbs.html";
+fl["6"]= "intro-network-lighting.html";
+fl["7"]= "intro-product.html";
+fl["8"]= "ref-remote-control-components.html";
+fl["9"]= "ref-specifications-led-bulb.html";
+fl["10"]= "ref-specifications-overview.html";
+fl["11"]= "ref-specifications-remote-control.html";
+fl["12"]= "ts-restart.html";
 var doStem = false;searchLoaded = true;
